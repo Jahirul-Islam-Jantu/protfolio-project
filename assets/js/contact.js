1d51581b128e5c21d41e6ed855b3f8ade92e8f0d
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-09-13 23:17:19 +0600

    contact

---
+++ assets/js/contact.js
@@ -0,0 +1,17 @@
+function submitHandler(event){
+    event.preventDefault();
+    const name = document.querySelector('#name').value
+    const email = document.querySelector('#mail').value
+    const phone = document.querySelector('#phone').value
+    const message = document.querySelector('#message').value
+    let result = document.querySelector('#showResult')
+
+    const jsonObj = {
+        name: name,
+        email: email,
+        phone: phone,
+        message: message,
+    }
+     result.innerText = JSON.stringify(jsonObj)
+
+}
